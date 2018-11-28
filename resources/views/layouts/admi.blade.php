da5315d20479d77db6fefa1858ee8fc0f34aa574
Nathaa <42421207+Nathaa@users.noreply.github.com>
2018-11-28 16:34:21 -0600

    asistencia sidebar

--- resources/views/layouts/admi.blade.php
+++ resources/views/layouts/admi.blade.php
@@ -50,7 +50,7 @@
   <header class="main-header">
 
     <!-- Logo -->
-    <a href="index2.html" class="logo">
+    <a href="#" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>A</b>LT</span>
       <!-- logo for regular state and mobile devices -->
@@ -298,6 +298,7 @@
                         <a class="nav-link" href="{{route('rangos.index')}}">Rangos</a>
             </li>
             @endcan
+            <li><a href="">Asistencia</a></li>
                   
                 </ul>
 
