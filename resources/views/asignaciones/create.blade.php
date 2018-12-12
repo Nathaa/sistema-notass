a10ab46d9e1e72eb623409bad4ee3390024a1849
arelibon94 <44760143+arelibon94@users.noreply.github.com>
2018-12-11 20:51:37 -0600

    Asignacion

--- resources/views/asignaciones/create.blade.php
+++ resources/views/asignaciones/create.blade.php
@@ -15,7 +15,7 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                 Inscripción a cursos
+                 Inscripción a curso
                 </div>
 
                 <div class="panel-body">
